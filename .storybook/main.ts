8ec9d39caab1e53751a2b078b00437a31c281f15
shellboy <qkrdmstlr3@naver.com>
2024-07-13 14:03:34 +0900

    init(system): 시스템 폴더 생성 및 스토리북 초기화 (#2)
    * init storybook
    
    * init system
    
    * set node version

---
+++ .storybook/main.ts
@@ -0,0 +1,25 @@
+import type { StorybookConfig } from '@storybook/nextjs';
+
+import { join, dirname } from 'path';
+
+// NOTE: Yarn Pnp를 사용할 때 필요
+function getAbsolutePath(value: string): any {
+  return dirname(require.resolve(join(value, 'package.json')));
+}
+
+const config: StorybookConfig = {
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
+    getAbsolutePath('@storybook/addon-onboarding'),
+    getAbsolutePath('@storybook/addon-links'),
+    getAbsolutePath('@storybook/addon-essentials'),
+    getAbsolutePath('@chromatic-com/storybook'),
+    getAbsolutePath('@storybook/addon-interactions'),
+  ],
+  framework: {
+    name: getAbsolutePath('@storybook/nextjs'),
+    options: {},
+  },
+  staticDirs: ['../public'],
+};
+export default config;
