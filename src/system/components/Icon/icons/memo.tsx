6e2a229975f8409a0ddd8cdc1f60f679af21540e
qkrdmstlr3 <qkrdmstlr3@naver.com>
2024-07-18 03:19:47 +0900

    fix snake case

--- src/system/components/Icon/icons/memo.tsx
+++ src/system/components/Icon/icons/memo.tsx
@@ -2,18 +2,13 @@ import { IconBaseType } from './type';
 
 export function Memo({ size, color }: IconBaseType) {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width={size}
-      height={size}
-      viewBox={`0 0 ${size} ${size}`}
-      fill="none">
+    <svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} viewBox={`0 0 ${size} ${size}`} fill="none">
       <path
         d="M3.93763 3.93763H20.0624V13.3384L13.3384 20.0624H3.93763V3.93763Z"
         stroke={color}
-        stroke-width="1.40651"
+        strokeWidth="1.40651"
       />
-      <path d="M12.5439 20V12.5439H20" stroke={color} stroke-width="1.40651" />
+      <path d="M12.5439 20V12.5439H20" stroke={color} strokeWidth="1.40651" />
     </svg>
   );
 }
--- src/system/components/Icon/icons/search.tsx
+++ src/system/components/Icon/icons/search.tsx
@@ -2,26 +2,9 @@ import { IconBaseType } from './type';
 
 export function Search({ size, color }: IconBaseType) {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width={size}
-      height={size}
-      viewBox={`0 0 ${size} ${size}`}
-      fill="none">
-      <circle
-        cx="10.9985"
-        cy="10.7888"
-        r="8.03854"
-        stroke={color}
-        stroke-width="1.5"
-        stroke-linecap="square"
-      />
-      <path
-        d="M16.4873 16.7084L21.0408 21.25"
-        stroke={color}
-        stroke-width="1.5"
-        stroke-linecap="square"
-      />
+    <svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} viewBox={`0 0 ${size} ${size}`} fill="none">
+      <circle cx="10.9985" cy="10.7888" r="8.03854" stroke={color} strokeWidth="1.5" strokeLinecap="square" />
+      <path d="M16.4873 16.7084L21.0408 21.25" stroke={color} strokeWidth="1.5" strokeLinecap="square" />
     </svg>
   );
 }
