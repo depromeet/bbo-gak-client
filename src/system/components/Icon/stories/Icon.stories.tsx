a4b9af603f23afe9940e1333bd3873b1cd3e5dab
shellboy <qkrdmstlr3@naver.com>
2024-07-18 22:15:13 +0900

    feat(system): Icon컴포넌트 구현 (#7)
    * feat(system): Icon컴포넌트 구현
    
    * fix snake case
    
    * fix name

---
+++ src/system/components/Icon/stories/Icon.stories.tsx
@@ -0,0 +1,24 @@
+import { Icon } from '../Icon';
+import { Meta } from '@storybook/react';
+
+const meta = {
+  title: 'Design System/Icon',
+  component: Icon,
+  tags: ['autodocs'],
+} satisfies Meta<typeof Icon>;
+
+export default meta;
+
+export const Example = () => {
+  return (
+    <>
+      <Icon name="bell" color="black" />
+      <Icon name="folder" color="black" />
+      <Icon name="logout" color="black" />
+      <Icon name="memo" color="black" />
+      <Icon name="profile" color="black" />
+      <Icon name="search" color="black" />
+      <Icon name="setting" color="black" />
+    </>
+  );
+};
