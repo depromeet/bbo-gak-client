456459fcbe41c3463393b00c631ce24672b4b0b7
Collection50 <fkaus65@naver.com>
2024-07-19 21:12:30 +0900

    shadcn

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,9 @@
 import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  darkMode: ['class'],
+  content: ['./pages/**/*.{ts,tsx}', './components/**/*.{ts,tsx}', './app/**/*.{ts,tsx}', './src/**/*.{ts,tsx}'],
+  prefix: '',
   theme: {
     extend: {
       backgroundImage: {
@@ -124,6 +122,7 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [require('tailwindcss-animate')],
 };
+
 export default config;
