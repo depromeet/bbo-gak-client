583bc458f4336e665c16f8a8ca2f508edc1ef1e4
eunbeann <ds5bean23@gmail.com>
2024-07-19 21:29:07 +0900

    feat: set the Provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Providers from '@/context/Providers';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
@@ -15,8 +16,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="ko">
+      <body className={inter.className}>
+        <Providers>
+          <div>{children}</div>
+        </Providers>
+      </body>
     </html>
   );
 }
---
+++ src/types/next-auth.d.ts
@@ -0,0 +1,8 @@
+import 'next-auth';
+
+declare module 'next-auth' {
+  interface Session {
+    userId: string;
+    accessToken: string;
+  }
+}
