f891cb9ae64df886228e74100f7b3208bcfbb611
Collection50 <fkaus65@naver.com>
2024-07-20 03:24:19 +0900

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
-import QueryProvider from '@/context/QueryProvider/QueryProvider';
 
 const inter = Inter({ subsets: ['latin'] });
 
