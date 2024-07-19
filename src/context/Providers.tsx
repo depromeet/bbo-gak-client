64b99d63124e6bd92e96230776f8c9a9ba5896aa
SeongMin Kim <86355699+Collection50@users.noreply.github.com>
2024-07-20 03:43:43 +0900

    HOTFIX: delte

--- src/context/Providers.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import type { PropsWithChildren } from 'react';
-import ReactQuery from './QueryProvider/ReactQuery';
-
-export default function Providers({ children }: PropsWithChildren) {
-  return <ReactQuery>{children}</ReactQuery>;
-}
