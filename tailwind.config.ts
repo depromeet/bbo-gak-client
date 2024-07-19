6400cf1521721e1755d4d7eff8cf296915bfd6de
Jungkyun Woo <73513965+woo-jk@users.noreply.github.com>
2024-07-19 22:54:42 +0900

    chore: tailwind 컬러 팔레트 설정 (#11)
    * tailwind 컬러 팔레트 설정
    
    * mint -> lime 네이밍 변경
    
    * palette 변경된 요소 반영

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,12 @@ const config: Config = {
   content: ['./pages/**/*.{ts,tsx}', './components/**/*.{ts,tsx}', './app/**/*.{ts,tsx}', './src/**/*.{ts,tsx}'],
   prefix: '',
   theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+    },
     colors: {
       white: '#FFFFFF',
       black: '#000000',
