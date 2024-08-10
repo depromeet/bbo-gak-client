f6235704d465596ba3dbe51a79c92ce590b5ff17
eunbeann <ds5bean23@gmail.com>
2024-08-11 04:08:55 +0900

    add: add types

---
+++ src/types/login.ts
@@ -0,0 +1,10 @@
+// NOTE : 추후 수정 예정 (임시 로그인)
+export interface PostLoginRequest {
+  loginId: string;
+  password: string;
+}
+
+export interface PostLoginResponse {
+  accessToken: string;
+  refreshToken: string;
+}
