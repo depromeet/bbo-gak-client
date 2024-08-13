48d4bddea2925130aeffc25eb2614c3f45762cb2
eunbeann <ds5bean23@gmail.com>
2024-08-13 12:00:21 +0900

    fix: change url

--- src/apis/login.ts
+++ src/apis/login.ts
@@ -3,7 +3,7 @@ import { http } from './http';
 
 export const postLogin = ({ loginId, password }: PostLoginRequest) => {
   return http.post<PostLoginResponse>({
-    url: '/api/v1/users/test/login',
+    url: '/users/test/login',
     data: {
       loginId,
       password,
