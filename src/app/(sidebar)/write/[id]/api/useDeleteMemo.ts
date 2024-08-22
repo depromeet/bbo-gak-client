dbd88ab99354acf2019b9c098240222565965f32
Collection50 <fkaus65@naver.com>
2024-08-22 23:41:52 +0900

    fix

--- src/app/(sidebar)/write/[id]/api/useDeleteMemo.ts
+++ src/app/(sidebar)/write/[id]/api/useDeleteMemo.ts
@@ -1,12 +1,12 @@
 import { http } from '@/apis/http';
 import { useMutation } from '@tanstack/react-query';
 
-export const deleteMemo = (cardId: string, memoId: number) =>
+export const deleteMemo = (cardId: number, memoId: number) =>
   http.delete({
     url: `/cards/${cardId}/card-memo/${memoId}`,
   });
 
-export const useDeleteMemo = (cardId: string) =>
+export const useDeleteMemo = (cardId: number) =>
   useMutation({
     mutationKey: ['delete-memo', cardId],
     mutationFn: (memoId: number) => deleteMemo(cardId, memoId),
