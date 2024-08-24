ae2fa0c3d66c6fc887618533ecb0c5325a4f562f
Collection50 <fkaus65@naver.com>
2024-08-24 15:25:51 +0900

    F

--- src/lib/QueryProvider/QueryProvider.tsx
+++ src/lib/QueryProvider/QueryProvider.tsx
@@ -1,9 +1,8 @@
 'use client';
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { useState } from 'react';
 import type { PropsWithChildren } from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import type { QueryClientConfig } from '@tanstack/react-query';
 
 const queryClientOption: QueryClientConfig = {
@@ -22,10 +21,5 @@ const queryClientOption: QueryClientConfig = {
 export function QueryProvider({ children }: PropsWithChildren) {
   const [queryClient] = useState(() => new QueryClient(queryClientOption));
 
-  return (
-    <QueryClientProvider client={queryClient}>
-      {children}
-      <ReactQueryDevtools />
-    </QueryClientProvider>
-  );
+  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
 }
