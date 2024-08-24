848dc60fff17d1b3ccb2836ef859fa1e68832b05
SeongMin Kim <86355699+Collection50@users.noreply.github.com>
2024-08-24 15:35:48 +0900

    feat(editor): memo 구현 (#30)
    * f
    
    * f'
    
    * F

--- src/app/(sidebar)/write/[id]/layout.tsx
+++ src/app/(sidebar)/write/[id]/layout.tsx
@@ -1,12 +1,15 @@
 import { AsyncBoundaryWithQuery } from '@/lib';
 import { PropsWithChildren } from 'react';
 import { CardTagFetcher } from './fetcher/CardTagFetcher';
+import { TagsFetcher } from './fetcher/TagsFetcher';
 
 export default function WriteLayout({ params: { id }, children }: PropsWithChildren<{ params: { id: string } }>) {
   return (
     <AsyncBoundaryWithQuery>
       <CardTagFetcher cardId={Number(id)}>
-        <main>{children}</main>
+        <TagsFetcher>
+          <main>{children}</main>
+        </TagsFetcher>
       </CardTagFetcher>
     </AsyncBoundaryWithQuery>
   );
