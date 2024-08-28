f3ff019c8de75be1fae8c7b880b984f476554f97
Collection50 <fkaus65@naver.com>
2024-08-29 01:46:29 +0900

    HOTFIX

--- src/app/(sidebar)/write/[id]/fetcher/TagsFetcher.tsx
+++ src/app/(sidebar)/write/[id]/fetcher/TagsFetcher.tsx
@@ -1,15 +1,18 @@
 'use client';
 
 import { generateContext } from '@/lib';
-import { Tag, useGetTags } from '../api/useGetTags/useGetTags';
+import { useGetTags } from '../api/useGetTags/useGetTags';
 import { StrictPropsWithChildren } from '@/types';
+import type { TagType } from '@/types';
 
-const [TagsProvider, useTagsContext] = generateContext<{ tags: Array<Tag> }>({ name: 'tags' });
+const [TagsProvider, useTagsContext] = generateContext<{ tags: Array<TagType> }>({ name: 'tags' });
 
 function TagsFetcher({ children }: StrictPropsWithChildren) {
   const { data } = useGetTags();
 
-  return <TagsProvider tags={data}>{children}</TagsProvider>;
+  // TODO: 새로고침했을 때는 data가 올바르고, 공고를 생성했을 때는 data.data가 올바름
+  // 완성을 위한 hacky한 방법
+  return <TagsProvider tags={data.data || data}>{children}</TagsProvider>;
 }
 
 export { TagsFetcher, useTagsContext };
