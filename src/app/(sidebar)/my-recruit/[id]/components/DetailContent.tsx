1b993bd434dae1225ef5b5b5bd442ea3399f691f
eunbeann <ds5bean23@gmail.com>
2024-08-29 02:07:08 +0900

    feat: tag name to id

--- src/app/(sidebar)/my-recruit/[id]/components/DetailContent.tsx
+++ src/app/(sidebar)/my-recruit/[id]/components/DetailContent.tsx
@@ -1,32 +1,36 @@
 'use client';
 
 import { InfoCard } from '@/components/InfoCard';
+import { TouchButton } from '@/components/TouchButton';
+import { Droppable } from '@/lib/dnd-kit/Droppable';
 import { Icon } from '@/system/components';
-import { color } from '@/system/token/color';
 import { InfoCardType } from '@/types';
 import { cn } from '@/utils/tailwind-util';
-import Link from 'next/link';
+import { motion } from 'framer-motion';
 import { useState } from 'react';
+import { usePostCardToRecruit } from '../../api/usePostCardToRecruit';
 import { useGetAllTags } from '../api/useGetAllTag';
 import { useGetCardCount } from '../api/useGetCardCount';
 import { useGetRecruitCards } from '../api/useGetRecruitCards';
+import { AddRecruitCardDialog } from './AddRecruitCardDialog';
 import TagList from './TagList';
 
-const PROGRESS_OPTIONS = ['서류_준비', '과제_준비', '인터뷰_준비'] as const;
+export const PROGRESS_OPTIONS = ['서류_준비', '과제_준비', '인터뷰_준비'] as const;
 
 export type ProgressType = (typeof PROGRESS_OPTIONS)[number];
 
 export function DetailContent({ recruitId }: { recruitId: string }) {
   const [currentOption, setCurrentOption] = useState<ProgressType>('서류_준비');
-  const [selectedTags, setSelectedTags] = useState<string[]>([]);
+  const [selectedTags, setSelectedTags] = useState<number[]>([]);
 
   const { data: cardCount } = useGetCardCount(recruitId);
   const { data: tagsData } = useGetAllTags();
-  const { data: cardList } = useGetRecruitCards({ id: recruitId, progress: currentOption });
+  const { data: cardList } = useGetRecruitCards({ id: recruitId, type: currentOption, tagIds: selectedTags });
+  const { mutate: mutatePostCardToRecruit } = usePostCardToRecruit();
 
   const filteredCardList =
     selectedTags.length > 0
-      ? cardList?.filter((card) => card.tagList.some((tag) => selectedTags.includes(tag.name)))
+      ? cardList?.filter((card) => card.tagList.some((tag) => selectedTags.includes(tag.id)))
       : cardList;
 
   return (
@@ -56,12 +60,17 @@ export function DetailContent({ recruitId }: { recruitId: string }) {
           })}
         </div>
 
-        <Link
-          className="flex items-center gap-[8px] bg-neutral-95 py-[0.5rem] px-[1rem] rounded-[6px]"
-          href={`/write/${recruitId}`}>
-          <Icon name="add" color={color.mint30} />
-          <div className="text-white text-[14px] font-semibold">카드 추가</div>
-        </Link>
+        <AddRecruitCardDialog>
+          <TouchButton layout>
+            <motion.div
+              initial={{ padding: '8px 16px' }}
+              variants={{ longPadding: { padding: '8px 16px' }, shortPadding: { padding: '8px 8px' } }}
+              className="bg-neutral-95 flex items-center gap-[4px] rounded-[6px]">
+              <Icon name="add" size={20} color="#20E79D" />
+              <span className="text-label1 text-white font-semibold">카드 추가</span>
+            </motion.div>
+          </TouchButton>
+        </AddRecruitCardDialog>
       </div>
 
       <TagList tagsData={tagsData || []} selectedTags={selectedTags} setSelectedTags={setSelectedTags} />
@@ -70,15 +79,19 @@ export function DetailContent({ recruitId }: { recruitId: string }) {
         {filteredCardList && filteredCardList.length > 0 ? (
           <ul className="grid grid-cols-[repeat(auto-fill,minmax(343px,1fr))] gap-[16px]">
             {filteredCardList.map((info: InfoCardType) => (
-              <li key={info.id} className="min-w-[343px]">
-                <InfoCard {...info} />
-              </li>
+              <Droppable key={info.id} id={info.id}>
+                <li className="min-w-[343px]">
+                  <InfoCard {...info} />
+                </li>
+              </Droppable>
             ))}
           </ul>
         ) : (
           <div className="flex flex-col w-full h-full justify-center items-center">
-            <Icon name="empty" size={280} />
-            <p className="mt-[16px] text-body1 font-neutral-30">아직 생성된 정보 카드가 없어요!</p>
+            <Droppable id={1234}>
+              <Icon name="empty" size={280} />
+              <p className="mt-[16px] text-body1 text-neutral-30">아직 생성된 정보 카드가 없어요!</p>
+            </Droppable>
           </div>
         )}
       </div>
