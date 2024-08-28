53186e14671c07f1c5f85504fcf307063607f505
eunbeann <ds5bean23@gmail.com>
2024-08-29 02:41:03 +0900

    feat: textBubble

--- src/app/(sidebar)/my-recruit/[id]/components/TextBubble.tsx
+++ src/app/(sidebar)/my-recruit/[id]/components/TextBubble.tsx
@@ -1,6 +1,6 @@
 import { Icon } from '@/system/components';
 import { cn } from '@/utils';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useGetRecruitById } from '../api/useGetRecruitById';
 import { usePatchSiteUrl } from '../api/usePatchSiteUrl';
 
@@ -17,52 +17,61 @@ export default function TextBubble({
   const { mutate: newLink } = usePatchSiteUrl();
 
   const [link, setLink] = useState('');
-  const [isLinkConfirmed, setIsLinkConfirmed] = useState(false);
-  const [isLinked, setIsLinked] = useState(recruitInfoById.siteUrl !== '');
+  const [isLinked, setIsLinked] = useState(false);
+
+  useEffect(() => {
+    console.log(isLinked);
+    if (recruitInfoById?.siteUrl) {
+      setLink(recruitInfoById.siteUrl);
+      setIsLinked(true);
+    } else {
+      setLink('');
+      setIsLinked(false);
+    }
+  }, [recruitInfoById]);
 
   const handleLinkConfirm = () => {
     if (link) {
       newLink({ newSiteUrl: link, id: recruitId });
-      setIsLinkConfirmed(true);
       linkedOn();
     }
+  };
+
+  const handleDeleteLink = () => {
+    setLink('');
+    newLink({ newSiteUrl: '', id: recruitId });
   };
 
   return (
     <div
       className={cn(
-        'absolute top-25 right-0 -left-115 bg-neutral-95 text-white text-sm py-2 px-2 rounded-md max-w-[225px]  min-w-[210px] transition-opacity duration-2000 hover:opacity-100',
+        'absolute top-25 right-0 -left-98 bg-neutral-95 text-white text-sm py-2 px-2 rounded-md max-w-[225px]  min-w-[210px] transition-opacity duration-2000 hover:opacity-100',
         isHovered ? 'opacity-100' : 'opacity-0',
       )}>
-      <div className="flex items-center gap-[4px] px-[12px] py-[6px] bg-neutral-95">
+      <div className="flex items-center justify-between gap-[4px] px-[12px] py-[6px] bg-neutral-95">
         <Icon name="link" size={16} />
-        {isLinked || isLinkConfirmed ? (
+        {isLinked ? (
           <>
             <a
               href={`/${link}`}
               target="_blank"
               rel="noopener noreferrer"
               className="text-label1 font-regular border-b-1 text-neutral-1 focus:outline-none focus:bg-neutral-95 focus:text-neutral-40 text-underlined max-w-[158px] truncate">
-              {recruitInfoById.siteUrl}
+              {link}
             </a>
-            <button
-              onClick={() => {
-                setLink('');
-                setIsLinkConfirmed(false);
-              }}
-              className={cn('mx-2 px-2 rounded-[2px]', 'hover:bg-neutral-75')}>
+            <button onClick={handleDeleteLink} className={cn('mx-2 px-2 rounded-[2px]', 'hover:bg-neutral-75')}>
               삭제
             </button>
           </>
         ) : (
           <>
             <input
-              className="text-label1 font-regular bg-neutral-95 text-neutral-1 focus:outline-none focus:bg-neutral-95 focus:text-neutral-40"
+              className="text-label1 font-regular bg-neutral-95 text-neutral-1 focus:bg-neutral-95 focus:text-neutral-40 "
               placeholder="공고 링크를 첨부해주세요"
               value={link}
               onChange={(e) => setLink(e.target.value)}
             />
-            <button onClick={handleLinkConfirm} className={cn('mx-2 p-2', link && 'hover:bg-neutral-75')}>
+            <button onClick={handleLinkConfirm} className={cn('mx-2 p-2 bg-neutral-95', link && 'hover:bg-neutral-75')}>
               <Icon name="check" size={16} color={link ? 'white' : 'opacity-100'} />
             </button>
           </>
