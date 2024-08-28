021985a694063ce7b4a00b4a6b89b71ccb644b5d
eunbeann <ds5bean23@gmail.com>
2024-08-29 02:06:36 +0900

    feat: change type

--- src/app/(sidebar)/my-recruit/[id]/api/useGetRecruitCards.ts
+++ src/app/(sidebar)/my-recruit/[id]/api/useGetRecruitCards.ts
@@ -1,25 +1,25 @@
 import { http } from '@/apis/http';
+import { TagType } from '@/types';
 import { useSuspenseQuery } from '@tanstack/react-query';
 
 export type GetRecruitCardsType = {
   id: number;
   title: string;
   updatedDate: string;
-  tagList: number[];
+  tagList: TagType[];
 };
 
-const getRecruitCards = ({ id, type, tagIds }: { id: string; type: string; tagIds: number[] }) => {
-  console.log(id, type, tagIds);
+const getRecruitCards = ({ id, type, tagIds }: { id: string; type: string; tagIds?: number[] }) => {
   return http.get<GetRecruitCardsType[]>({
     url: `recruits/${id}/cards`,
     params: {
       type: type,
-      'tag-ids': tagIds.join(','),
+      'tag-ids': tagIds?.join(','),
     },
   });
 };
 
-export function useGetRecruitCards({ id, type, tagIds }: { id: string; type: string; tagIds: number[] }) {
+export function useGetRecruitCards({ id, type, tagIds }: { id: string; type: string; tagIds?: number[] }) {
   const result = useSuspenseQuery({
     queryKey: ['get-recruit-card-id', id, type, tagIds],
     queryFn: async () => {
