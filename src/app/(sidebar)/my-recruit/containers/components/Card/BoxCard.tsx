0f17486757b7e00febbfa418401944454428410e
EunBean Kwon <65286685+eunbeann@users.noreply.github.com>
2024-08-29 11:03:31 +0900

    내 공고에서 내 공고 디테일로 라우팅 연결 (#45)
    * feat: add router
    
    * feat: add import

--- src/app/(sidebar)/my-recruit/containers/components/Card/BoxCard.tsx
+++ src/app/(sidebar)/my-recruit/containers/components/Card/BoxCard.tsx
@@ -5,6 +5,7 @@ import { dday } from '@/utils/date';
 import { MoreButton } from '@/app/(sidebar)/my-recruit/containers/components/Card/common/MoreButton';
 import { StatusButton } from '@/app/(sidebar)/my-recruit/containers/components/Card/common/StatusButton';
 import { Dialog } from '@/system/components/Dialog/ShadcnDialog';
+import Link from 'next/link';
 import { DueDateDialog } from '../DueDateDialog';
 import { RecruitCard } from '@/app/(sidebar)/my-recruit/type';
 
@@ -29,41 +30,43 @@ export function BoxCard({
   const minWidth = MIN_CARD_WIDTH;
 
   return (
-    <div className="flex-1 rounded-[10px] overflow-hidden cursor-pointer" style={{ minWidth }}>
-      <div className="h-38 pr-12 pl-20 bg-neutral-95 flex justify-between items-center">
-        {nearestSchedule == null ? (
-          <Dialog>
-            <Dialog.Trigger className="flex justify-between items-center w-full">
-              <span className="text-label2 text-neutral-50">공고 일정을 등록해주세요</span>
-              <Icon name="add" size={24} color={color.neutral50} />
-            </Dialog.Trigger>
-            <Dialog.Content className="w-400">
-              <DueDateDialog title={title} onDuedateAppend={() => onDuedateAppend()} />
-            </Dialog.Content>
-          </Dialog>
-        ) : (
-          <>
-            <div className="flex items-center gap-[4px]">
-              <Icon name="clover" size={20} color={color.mint30} />
-              <span className="text-white text-label2 ">
-                {nearestSchedule.recruitScheduleStage} D-{dday(nearestSchedule.deadLine)}
-              </span>
-            </div>
-            <MoreButton onDeleteClick={() => onRecruitDelete(id)} />
-          </>
-        )}
-      </div>
-      <div className="p-20 pt-16 bg-white border-neutral-5 border-1 rounded-b-[10px] hover:border-neutral-95">
-        <div className="flex justify-between items-center">
-          <div className="bg-mint-1 text-mint-50 text-label1 px-8 py-4 rounded-4">{season}</div>
-          <StatusButton
-            currentStatus={recruitStatus}
-            onRecruitStatusChange={(status) => onRecruitStatusChange(id, status)}
-          />
+    <Link href={`/my-recruit/${id}`}>
+      <div className="flex-1 rounded-[10px] overflow-hidden cursor-pointer" style={{ minWidth }}>
+        <div className="h-38 pr-12 pl-20 bg-neutral-95 flex justify-between items-center">
+          {nearestSchedule == null ? (
+            <Dialog>
+              <Dialog.Trigger className="flex justify-between items-center w-full">
+                <span className="text-label2 text-neutral-50">공고 일정을 등록해주세요</span>
+                <Icon name="add" size={24} color={color.neutral50} />
+              </Dialog.Trigger>
+              <Dialog.Content className="w-400">
+                <DueDateDialog title={title} onDuedateAppend={() => onDuedateAppend()} />
+              </Dialog.Content>
+            </Dialog>
+          ) : (
+            <>
+              <div className="flex items-center gap-[4px]">
+                <Icon name="clover" size={20} color={color.mint30} />
+                <span className="text-white text-label2 ">
+                  {nearestSchedule.recruitScheduleStage} D-{dday(nearestSchedule.deadLine)}
+                </span>
+              </div>
+              <MoreButton onDeleteClick={() => onRecruitDelete(id)} />
+            </>
+          )}
         </div>
-        <Spacing size={16} />
-        <span className="text-nuetral-95 font-semibold font-body1 text-ellipsis line-clamp-1">{title}</span>
+        <div className="p-20 pt-16 bg-white border-neutral-5 border-1 rounded-b-[10px] hover:border-neutral-95">
+          <div className="flex justify-between items-center">
+            <div className="bg-mint-1 text-mint-50 text-label1 px-8 py-4 rounded-4">{season}</div>
+            <StatusButton
+              currentStatus={recruitStatus}
+              onRecruitStatusChange={(status) => onRecruitStatusChange(id, status)}
+            />
+          </div>
+          <Spacing size={16} />
+          <span className="text-nuetral-95 font-semibold font-body1 text-ellipsis line-clamp-1">{title}</span>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 }
--- src/app/(sidebar)/my-recruit/containers/components/Card/RowCard.tsx
+++ src/app/(sidebar)/my-recruit/containers/components/Card/RowCard.tsx
@@ -4,10 +4,11 @@ import { Spacing } from '@/system/utils/Spacing';
 import { Icon } from '@/system/components';
 import { color } from '@/system/token/color';
 import { dday } from '@/utils/date';
-import { MoreButton } from '@/app/(sidebar)/my-recruit/containers/components/Card/common/MoreButton';
+import Link from 'next/link';
 import { StatusButton } from './common/StatusButton';
 import { cn } from '@/utils';
 import { RecruitCard } from '@/app/(sidebar)/my-recruit/type';
+import { MoreButton } from './common/MoreButton';
 
 type RowCardProps = RecruitCard & {
   highlighted?: boolean;
@@ -34,36 +35,38 @@ export function RowCard({
     .otherwise(() => 'bg-neutral-95');
 
   return (
-    <div className={cn('w-full rounded-[10px] overflow-hidden flex cursor-pointer', pointerEventsClassName)}>
-      <div className={cn('w-12 disabled:bg-neutral-10 h-70', rightMarkBackgroundColorClassName)} />
-      <div
-        className={cn(
-          'px-24 py-22 flex-1 flex items-center border-1 border-l-transparent rounded-r-[10px] justify-between hover:border-neutral-95 hover:border-l-transparent',
-          highlighted ? 'border-mint-10 bg-[rgba(221,243,235,0.50)]' : 'border-neutral-5 bg-[white]',
-        )}>
-        <div className="flex items-center">
-          <span className="text-neutral-50 text-label2 font-medium">{season}</span>
-          <Spacing size={24} direction="row" />
-          <If condition={nearestSchedule != null}>
-            <div className="px-6 py-4 rounded-[4px] bg-neutral-95 flex gap-[2px]">
-              <Icon name="clover" size={20} color={color.mint30} />
-              <span className="text-white text-label2 ">
-                {recruitStatus} D-{dday(nearestSchedule?.deadLine!)}
-              </span>
-            </div>
-          </If>
-          <Spacing size={12} direction="row" />
-          <span className="font-semibold font-body1">{title}</span>
-        </div>
-        <div className="flex items-center">
-          <StatusButton
-            currentStatus={recruitStatus}
-            onRecruitStatusChange={(status) => onRecruitStatusChange(id, status)}
-          />
-          <Spacing size={32} direction="row" />
-          <MoreButton onDeleteClick={() => onRecruitDelete(id)} />
+    <Link href={`/my-recruit/${id}`}>
+      <div className={cn('w-full rounded-[10px] overflow-hidden flex cursor-pointer', pointerEventsClassName)}>
+        <div className={cn('w-12 disabled:bg-neutral-10 h-70', rightMarkBackgroundColorClassName)} />
+        <div
+          className={cn(
+            'px-24 py-22 flex-1 flex items-center border-1 border-l-transparent rounded-r-[10px] justify-between hover:border-neutral-95 hover:border-l-transparent',
+            highlighted ? 'border-mint-10 bg-[rgba(221,243,235,0.50)]' : 'border-neutral-5 bg-[white]',
+          )}>
+          <div className="flex items-center">
+            <span className="text-neutral-50 text-label2 font-medium">{season}</span>
+            <Spacing size={24} direction="row" />
+            <If condition={nearestSchedule != null}>
+              <div className="px-6 py-4 rounded-[4px] bg-neutral-95 flex gap-[2px]">
+                <Icon name="clover" size={20} color={color.mint30} />
+                <span className="text-white text-label2 ">
+                  {recruitStatus} D-{dday(nearestSchedule?.deadLine!)}
+                </span>
+              </div>
+            </If>
+            <Spacing size={12} direction="row" />
+            <span className="font-semibold font-body1">{title}</span>
+          </div>
+          <div className="flex items-center">
+            <StatusButton
+              currentStatus={recruitStatus}
+              onRecruitStatusChange={(status) => onRecruitStatusChange(id, status)}
+            />
+            <Spacing size={32} direction="row" />
+            <MoreButton onDeleteClick={() => onRecruitDelete(id)} />
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 }
