5465114ea324f0f458ce607201761e5c566dbdb5
eunbeann <ds5bean23@gmail.com>
2024-08-29 10:53:53 +0900

    feat: add router

--- src/app/(sidebar)/my-recruit/containers/components/Card/RowCard.tsx
+++ src/app/(sidebar)/my-recruit/containers/components/Card/RowCard.tsx
@@ -34,36 +34,38 @@ export function RowCard({
     .otherwise(() => 'bg-neutral-95');
 
   return (
-    <div className={cn('w-full rounded-[10px] overflow-hidden flex cursor-pointer', pointerEventsClassName)}>
-      <div className={cn('w-12 disabled:bg-neutral-10 h-70', rightMarkBackgroundColorClassName)} />
-      <div
-        className={cn(
-          'px-24 py-22 flex-1 flex items-center border-1 border-l-transparent rounded-r-[10px] justify-between hover:border-neutral-95 hover:border-l-transparent',
-          highlighted ? 'border-mint-10 bg-[rgba(221,243,235,0.50)]' : 'border-neutral-5 bg-[white]',
-        )}>
-        <div className="flex items-center">
-          <span className="text-neutral-50 text-label2 font-medium">{season}</span>
-          <Spacing size={24} direction="row" />
-          <If condition={nearestSchedule != null}>
-            <div className="px-6 py-4 rounded-[4px] bg-neutral-95 flex gap-[2px]">
-              <Icon name="clover" size={20} color={color.mint30} />
-              <span className="text-white text-label2 ">
-                {recruitStatus} D-{dday(nearestSchedule?.deadLine!)}
-              </span>
-            </div>
-          </If>
-          <Spacing size={12} direction="row" />
-          <span className="font-semibold font-body1">{title}</span>
-        </div>
-        <div className="flex items-center">
-          <StatusButton
-            currentStatus={recruitStatus}
-            onRecruitStatusChange={(status) => onRecruitStatusChange(id, status)}
-          />
-          <Spacing size={32} direction="row" />
-          <MoreButton onDeleteClick={() => onRecruitDelete(id)} />
+    <Link href={`/my-recruit/${id}`}>
+      <div className={cn('w-full rounded-[10px] overflow-hidden flex cursor-pointer', pointerEventsClassName)}>
+        <div className={cn('w-12 disabled:bg-neutral-10 h-70', rightMarkBackgroundColorClassName)} />
+        <div
+          className={cn(
+            'px-24 py-22 flex-1 flex items-center border-1 border-l-transparent rounded-r-[10px] justify-between hover:border-neutral-95 hover:border-l-transparent',
+            highlighted ? 'border-mint-10 bg-[rgba(221,243,235,0.50)]' : 'border-neutral-5 bg-[white]',
+          )}>
+          <div className="flex items-center">
+            <span className="text-neutral-50 text-label2 font-medium">{season}</span>
+            <Spacing size={24} direction="row" />
+            <If condition={nearestSchedule != null}>
+              <div className="px-6 py-4 rounded-[4px] bg-neutral-95 flex gap-[2px]">
+                <Icon name="clover" size={20} color={color.mint30} />
+                <span className="text-white text-label2 ">
+                  {recruitStatus} D-{dday(nearestSchedule?.deadLine!)}
+                </span>
+              </div>
+            </If>
+            <Spacing size={12} direction="row" />
+            <span className="font-semibold font-body1">{title}</span>
+          </div>
+          <div className="flex items-center">
+            <StatusButton
+              currentStatus={recruitStatus}
+              onRecruitStatusChange={(status) => onRecruitStatusChange(id, status)}
+            />
+            <Spacing size={32} direction="row" />
+            <MoreButton onDeleteClick={() => onRecruitDelete(id)} />
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 }
