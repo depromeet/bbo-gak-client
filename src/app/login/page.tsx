882ad9b669948502b8835bc6910aa94178541219
Collection50 <fkaus65@naver.com>
2024-08-29 17:16:15 +0900

    f

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,11 +5,12 @@ import { Login } from './components/Login';
 import { Redirect } from '@/components/Redirect';
 import Select from './components/Select';
 import { getCookie } from 'cookies-next';
+import { ACCESS_TOKEN, JOB_SELECTION, SELECT } from './constants/token';
 
 export default function Page() {
   const Funnel = useFunnel(['login', 'select'] as const, { initialStep: 'login', stepQueryKey: 'auth' });
-  const isSelectJob = getCookie('jobSelection') === 'select';
-  const accessToken = !!getCookie('accessToken');
+  const isSelectJob = getCookie(JOB_SELECTION) === SELECT;
+  const accessToken = !!getCookie(ACCESS_TOKEN);
 
   return (
     <Funnel mode="wait">
