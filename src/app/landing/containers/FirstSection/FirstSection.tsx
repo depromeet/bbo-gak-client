5a22190a5b3d0240418702d5f35d4f5fce9ff5b8
shellboy <qkrdmstlr3@naver.com>
2024-08-30 11:59:32 +0900

    Feat/landing2 (#52)
    * feat(landing): 랜딩페이지 구현
    
    * save

--- src/app/landing/containers/FirstSection/FirstSection.tsx
+++ src/app/landing/containers/FirstSection/FirstSection.tsx
@@ -1,18 +1,23 @@
 import { Spacing } from '@/system/utils/Spacing';
 import { DownArrow } from './components/DownArrow';
+import Image from 'next/image';
 
 export function FirstSection() {
   return (
-    <div className="bg-neutral-95 h-[100vh] flex flex-col items-center justify-end pb-[30px]">
-      <span className="text-white font-[700] text-[48px]">아.. 그때 써놓은 자소서 항목 어딨더라?</span>
-      <Spacing size={24} />
-      <span className="text-neutral-30 font-[500] text-[24px] whitespace-pre text-center">
-        {`취준 중 모으는 수많은 정보들, 막상 필요할 때\n어디 있었는지 찾기 힘들었던 경험 있으셨나요?`}
-      </span>
-      <Spacing size={10} />
+    <div className="bg-neutral-95 h-[100vh] flex flex-col items-center justify-between pt-[88px] pb-[30px]">
+      <Image src="/landing/landing_scape.png" alt="" width={1440} height={900} className="z-[0] absolute top-0" />
+      <div className="flex flex-col items-center">
+        <span className="text-white font-[700] text-[48px]">아.. 그때 써놓은 자소서 항목 어딨더라?</span>
+        <Spacing size={24} />
+        <span className="text-neutral-30 font-[500] text-[24px] whitespace-pre text-center">
+          {`취준 중 모으는 수많은 정보들, 막상 필요할 때\n어디 있었는지 찾기 힘들었던 경험 있으셨나요?`}
+        </span>
+      </div>
 
-      <DownArrow />
-      <Spacing size={60} />
+      <div>
+        <DownArrow />
+        <Spacing size={60} />
+      </div>
     </div>
   );
 }
--- src/app/landing/containers/FourthSection/FourthSection.tsx
+++ src/app/landing/containers/FourthSection/FourthSection.tsx
@@ -14,8 +14,8 @@ export function FourthSection() {
       <Spacing size={24} />
       <span className="text-neutral-50 text-title2 whitespace-pre text-center">{`취준 정보를 작성하는 순간부터 태그를 등록하여\n원하는 키워드의 정보를 빠르게 찾을 수 있어요`}</span>
       <Spacing size={100} />
-      <div className="rounded-[16px] border-neutral-95 border-[8px] overflow-hidden mx-[20px]">
-        <Image src="/landing/image156.png" alt="" width={1000} height={640} />
+      <div className="rounded-[30px] border-neutral-95 border-[8px] overflow-hidden mx-[20px]">
+        <Image src="/landing/image156_2.png" alt="" width={1000} height={640} />
       </div>
     </div>
   );
