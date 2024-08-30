2c1181bbb475c854c7c09f08ca18408bf4809e91
SeongMin Kim <86355699+Collection50@users.noreply.github.com>
2024-08-31 03:03:56 +0900

    feat(font): 폰트 설정 (#61)
    * feat
    
    * Fix

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -10,11 +10,12 @@ import { ACCESS_TOKEN, JOB_SELECTION, SELECT } from './constants/token';
 export default function Page() {
   const Funnel = useFunnel(['login', 'select'] as const, { initialStep: 'login', stepQueryKey: 'auth' });
   const isSelectJob = getCookie(JOB_SELECTION) === SELECT;
+  const accessToken = getCookie(ACCESS_TOKEN);
 
   return (
     <Funnel mode="wait">
       <Funnel.Step name="login">
-        <Redirect condition={!isSelectJob} to="/login?auth=select">
+        <Redirect condition={accessToken != null && !isSelectJob} to="/login?auth=select">
           <Login />
         </Redirect>
       </Funnel.Step>
