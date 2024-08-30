5b62a6980acd97d64b25ab9305647c5d3f9f7d09
Collection50 <fkaus65@naver.com>
2024-08-30 23:06:41 +0900

    fix

--- src/app/login/layout.tsx
+++ src/app/login/layout.tsx
@@ -1,5 +1,10 @@
 import { StrictPropsWithChildren } from '@/types';
+import { Suspense } from 'react';
 
 export default async function Layout({ children }: StrictPropsWithChildren) {
-  return <main className="h-screen flex justify-center items-center bg-neutral-95">{children}</main>;
+  return (
+    <main className="h-screen flex justify-center items-center bg-neutral-95">
+      <Suspense>{children}</Suspense>
+    </main>
+  );
 }
