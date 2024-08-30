0f36683090a530628177af87217b6817816593ba
qkrdmstlr3 <qkrdmstlr3@naver.com>
2024-08-31 04:51:17 +0900

    feat(onboarding): 캐시 제거

--- src/app/(sidebar)/(my-info)/apis/usePutOnboardStatus.ts
+++ src/app/(sidebar)/(my-info)/apis/usePutOnboardStatus.ts
@@ -1,15 +1,21 @@
 import { http } from '@/apis/http';
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { GET_ONBOARD_STATUS } from '@/app/(sidebar)/(my-info)/apis/useGetOnboadStatus';
 
 const putOnboardStatus = () => {
   return http.put<Response>({ url: `/users/onboard-status`, data: { isOnboardComplete: true } });
 };
 
 export const usePutOnboardStatus = () => {
+  const queryClient = useQueryClient();
+
   const mutate = useMutation({
     mutationFn: async () => {
       const res = await putOnboardStatus();
       return res.data;
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: [GET_ONBOARD_STATUS] });
     },
   });
 
