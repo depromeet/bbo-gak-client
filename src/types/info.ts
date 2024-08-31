4deae5ed88517fc91789da7ff485e366abc76977
eunbeann <ds5bean23@gmail.com>
2024-08-31 14:25:05 +0900

    restore: restore TITLE

--- src/types/info.ts
+++ src/types/info.ts
@@ -14,7 +14,7 @@ export interface TagType {
 export const INFO_TYPES = ['경험_정리', '자기소개서', '면접_질문'] as const;
 export type InfoType = (typeof INFO_TYPES)[number];
 
-export const ANNOUNCEMENT_TYPES = ['서류_준비', '과제_준비', '면접_준비', '내_정보_복사'] as const;
+export const ANNOUNCEMENT_TYPES = ['서류_준비', '과제_준비', '인터뷰_준비', '내_정보_복사'] as const;
 
 export const TYPE_LIST = [...INFO_TYPES, ...ANNOUNCEMENT_TYPES] as const;
 export type TypeTag = (typeof TYPE_LIST)[number];
