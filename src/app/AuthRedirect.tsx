421c0d33c16877087e82909993921c4dccced6b8
SeongMin Kim <86355699+Collection50@users.noreply.github.com>
2024-09-08 00:25:29 +0900

    fix(login): 로그인 로직 오류 수정 (#82)
    * fix
    
    * fix
    
    * fix

--- src/app/AuthRedirect.tsx
+++ src/app/AuthRedirect.tsx
@@ -1,16 +1,18 @@
-import { cookies } from 'next/headers';
+'use client';
+
 import { ACCESS_TOKEN, JOB_SELECTION, REFRESH_TOKEN } from './login/constants/token';
 import { redirect } from 'next/navigation';
 import type { StrictPropsWithChildren } from '@/types';
+import { deleteCookie, getCookie } from 'cookies-next';
 
-export default async function AuthRedirect({ children }: StrictPropsWithChildren) {
-  const accessToken = cookies().get(ACCESS_TOKEN)?.value;
-  const isJobSelection = cookies().get(JOB_SELECTION)?.value;
+export default function AuthRedirect({ children }: StrictPropsWithChildren) {
+  const accessToken = getCookie(ACCESS_TOKEN) as string;
+  const isJobSelection = getCookie(JOB_SELECTION) as string;
 
   if (!accessToken || !isJobSelection) {
-    cookies().delete(ACCESS_TOKEN);
-    cookies().delete(REFRESH_TOKEN);
-    cookies().delete(JOB_SELECTION);
+    deleteCookie(ACCESS_TOKEN);
+    deleteCookie(REFRESH_TOKEN);
+    deleteCookie(JOB_SELECTION);
     redirect('/login');
   }
 
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,17 +5,16 @@ import { useFunnel } from '@/system/components/Funnel/useFunnel';
 import { getCookie } from 'cookies-next';
 import { Login } from './components/Login';
 import Select from './components/Select';
-import { ACCESS_TOKEN, JOB_SELECTION, SELECT } from './constants/token';
+import { JOB_SELECTION, SELECT } from './constants/token';
 
 export default function Page() {
   const Funnel = useFunnel(['login', 'select'] as const, { initialStep: 'login', stepQueryKey: 'auth' });
   const isSelectJob = getCookie(JOB_SELECTION) === SELECT;
-  const accessToken = getCookie(ACCESS_TOKEN);
 
   return (
     <Funnel mode="wait">
       <Funnel.Step name="login">
-        <Redirect condition={accessToken != null && !isSelectJob} to="/login?auth=select">
+        <Redirect condition={!isSelectJob} to="/login?auth=select">
           <Login />
         </Redirect>
       </Funnel.Step>
