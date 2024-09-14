066e20f5a446fcabdc55c09a7c8c12d4a1efbf95
Collection50 <fkaus65@naver.com>
2024-09-14 13:11:55 +0900

    fix

--- src/app/(sidebar)/write/[id]/components/MemoContainer/Memo/DeleteMemoDialog.tsx
+++ src/app/(sidebar)/write/[id]/components/MemoContainer/Memo/DeleteMemoDialog.tsx
@@ -18,9 +18,9 @@ export function DeleteMemoDialog({
 
       <DialogContent className="border-1 w-460 h-232 p-32 pt-24 flex flex-col gap-36">
         <div className="mt-36 text-20 font-bold text-center flex flex-col items-center justify-center">
-          <div className="flex">
+          <div className="flex w-fit gap-5">
             <span>'</span>
-            <p className="w-370 overflow-hidden text-ellipsis whitespace-nowrap">{memo}</p>
+            <p className="max-w-370 w-fit overflow-hidden text-ellipsis whitespace-nowrap">{memo}</p>
             <span>'</span>
           </div>
           <p>메모를 정말 삭제하시겠습니까?</p>
